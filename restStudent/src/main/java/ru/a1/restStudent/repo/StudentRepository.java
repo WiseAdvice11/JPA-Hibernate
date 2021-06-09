adaebbabd1d3e77762efca73964ede93cdaa09da
WiseAdvice11 <69453977+WiseAdvice11@users.noreply.github.com>
2021-06-09 23:08:20 +0300

    Delete restStudent/src/main/java/ru/a1/restStudent directory

--- restStudent/src/main/java/ru/a1/restStudent/repo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.a1.restStudent.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.a1.restStudent.entity.Student;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student,Long> {
-}
